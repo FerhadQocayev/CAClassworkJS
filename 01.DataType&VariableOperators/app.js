2126fadfcaff8051a4a68f68b56aa155e5fb42fd
FerhadQocayev <qocayevfrhad234@gmail.com>
2023-10-30 08:55:32 -0700

    First JS task

---
+++ 01.DataType&VariableOperators/app.js
@@ -0,0 +1,70 @@
+//task 1
+
+let firstName = "Ferhad";
+let lastName = "Qocayev.";
+let fullName = firstName + " " + lastName;
+console.log(fullName);
+let country = "I live in Azerbaijan.";
+let age = "I am 20.";
+let job = "none.";
+let all = country + " " + age + " " + job;
+let together = fullName + " " + all;
+console.log(together);
+
+//task 2
+
+let length = 16; //number
+console.log(typeof length);
+let color = "Brown"; //string
+console.log(typeof color);
+let float = "12.5"; //string
+console.log(typeof float);
+let like = true; //boolean
+console.log(typeof like);
+let x = {
+  firstName: "Nick",
+  color: "Red",
+}; //object
+console.log(typeof x);
+
+//task 3
+
+let distance = 8600;
+let speed = 120;
+let duration = distance / speed;
+console.log(
+  "The car will arrive in somewhere in " + duration + " minutes later."
+);
+
+//task 4
+
+let birth = 2004;
+let year1 = 2023;
+let year2 = 2024;
+let myage1 = year1 - birth;
+let myage2 = year2 - birth;
+
+console.log("I will be either " + myage1 + " or" + myage2 + ".");
+
+//task 5
+
+let revan = 45;
+let adil = 50;
+let samir = 35;
+let ilahe = 25;
+let nuran = 19;
+let sum = revan + adil + samir + ilahe + nuran;
+let average = sum / 5;
+console.log("The average point is : " + average);
+
+//task 6
+
+let admin;
+let name = "John";
+admin = name;
+window.alert(admin);
+
+//task 7
+
+let ourplanet = "Earth";
+let username = "John";
