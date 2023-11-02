ef2207259ff2fb34296c657b2f310ed9803eb400
FerhadQocayev <qocayevfrhad234@gmail.com>
2023-11-02 08:44:22 -0700

    Task finished

---
+++ 02.ControlStructure/main.js
@@ -0,0 +1,24 @@
+// task 1
+
+// for (let i = 1; i <= 10; i++) {
+//   console.log(Math.round(Math.random() * 30 + 20));
+// }
+
+// task 2
+
+for (let i = 1; i <= 1; i++) {
+  //   console.log(Math.round(Math.random() * 255 + 0));
+  console.log(
+    `rgb(${Math.round(Math.random() * 255 + 0)}, ${Math.round(
+      Math.random() * 255 + 0
+    )}, ${Math.round(Math.random() * 255 + 0)})`
+  );
+}
+for (let i = 1; i <= 1; i++) {
+  //   console.log(Math.round(Math.random() * 255 + 0));
+  console.log(
+    `rgba(${Math.round(Math.random() * 255 + 0)}, ${Math.round(
+      Math.random() * 255 + 0
+    )}, ${Math.round(Math.random() * 255 + 0)},${Math.random().toFixed(1)})`
+  );
+}
